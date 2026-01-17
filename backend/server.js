1174d83183cded20210789492c27de58f3dbc166
Dechayx <dechayxthei@gmail.com>
2026-01-17 11:00:35 +0530

    fix for vercel json

--- backend/server.js
+++ backend/server.js
@@ -239,4 +239,4 @@ app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
 });
 
-app.listen(5000, () => console.log(`🚀 Server on http://localhost:5000`));
+module.exports = app;
